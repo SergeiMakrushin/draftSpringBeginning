12a49a079701bd58e5f0bfea085fd9daa7268f60
Сергей <mak.sergei@ngs.ru>
2023-04-30 19:40:23 +0700

    Initial commit

---
+++ src/main/java/com/example/draftspringbeginning/DraftSpringBeginningApplication.java
@@ -0,0 +1,13 @@
+package com.example.draftspringbeginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftSpringBeginningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraftSpringBeginningApplication.class, args);
+    }
+
+}
